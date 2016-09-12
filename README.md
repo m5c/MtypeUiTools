cbb6f0812f464613a6d57f61e05f4d403ca79a07
m5c <schieder@fim.uni-passau.de>
2016-09-12 15:39:13 -0400

    lib setup

---
+++ README.md
@@ -0,0 +1,55 @@
+# README #
+
+## About ##
+
+### In one sentence ###
+
+Providing good looking Swing UIComponents that should have been in the box.
+
+### A little more & motivation ###
+
+* Though using Swing and AWT many retty things can be somehow created, I found myself using some patterns over and over again. This library aims to simplify reuse of these custom components.
+
+## Features ##
+
+### Pro ###
+
+* Good looking, custom background, anti-aliased toggelable buttons. 
+
+### Con ###
+
+## How do I get set up? ##
+
+### Old School ###
+* Checkout this repo
+* Integrate the included sources (src) or JAR (target) into your project
+
+### Maven ###
+* Add the following to your pom.xml (somewhere within the ```<project>``` block)
+
+```
+<repositories>
+	[...]
+	<repository>
+		<id>jitpack.io</id>
+		<url>https://jitpack.io</url>
+	</repository>
+	[...]
+</repositories>
+
+[...]
+
+<dependencies>
+	[...]
+	<dependency>
+		<groupId>com.github.m5c</groupId>
+		<artifactId>mtypeuitools</artifactId>
+		<version>-SNAPSHOT</version>
+	</dependency>
+	[...]
+</dependencies>
+```
+
+## How do I use SafeSockets? ##
+
+Either use the components as provided, or inherite from them. The rest is up to you.
---
+++ src/main/java/com/m5c/mtypeuitools/RectButton.java
@@ -0,0 +1,88 @@
+package main.java.com.m5c.mtypeuitools;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.Rectangle2D;
+import javax.swing.JPanel;
+
+/*
+ * @Author m5c
+ */
+public abstract class RectButton extends JPanel
+{
+
+    String bgColorCode;
+    String fgColorCode;
+
+    String iaColorCode;
+
+    private final String text;
+    private boolean active;
+    private final boolean isCategory;
+
+    public RectButton(String text, boolean active, boolean toggelable, boolean isCategory, MouseListener categoryToggleListener)
+    {
+        this.text = text;
+        this.active = active;
+        this.isCategory = isCategory;
+
+        if (toggelable)
+            addMouseListener(categoryToggleListener);
+    }
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        //initialize font (will still be drawn after areas and rims)
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        int fontSize = (int) (getHeight() * 0.5);
+        Font font = new Font("Sans", Font.PLAIN, fontSize);
+        g2.setFont(font);
+        FontMetrics fm = g2.getFontMetrics();
+        Rectangle2D r = fm.getStringBounds(text, g2);
+        int labelPosX = (this.getWidth() - (int) r.getWidth()) / 2;
+        int labelPosY = (this.getHeight() / 2 - (int) r.getHeight()) / 2 + (int) (1.44 * fm.getAscent());
+
+        //draw background
+        g2.setColor(Color.decode(bgColorCode));
+        g2.fillRect(0, 0, getWidth(), getHeight());
+        // set color for rim and fomt
+        if (!active)
+            g2.setColor(Color.decode(iaColorCode));
+        else
+            g2.setColor(Color.decode(fgColorCode));
+        // draw rim
+        g2.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
+        // draw font (same color as rim)
+        g2.drawString(text, labelPosX, labelPosY);
+    }
+
+    public boolean isActive()
+    {
+        return active;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setActivity(boolean active)
+    {
+        this.active = active;
+        repaint();
+    }
+    
+    public void onModelUpdate()
+    {
+        repaint();
+    }
+}
